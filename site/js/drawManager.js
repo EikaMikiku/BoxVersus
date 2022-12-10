7c77240402194cc90ff91b557831d06be966bc90
EikaMikiku <e.i.k.a@outlook.com>
2022-12-10 16:31:30 +0000

    Now can draw boxes. Added count down for the round.

---
+++ site/js/drawManager.js
@@ -0,0 +1,87 @@
+function DrawManager(onBoxDraw, getBoxes) {
+	const HITBOX = "red";
+	const HURTBOX = "lime";
+
+	let DrawingBGCanvas = document.getElementById("DrawingBGCanvas");
+	let DrawingBoxesCanvas = document.getElementById("DrawingBoxesCanvas");
+	let DrawingPreviewCanvas = document.getElementById("DrawingPreviewCanvas");
+
+	let previewCtx = DrawingPreviewCanvas.getContext("2d");
+	let boxesCtx = DrawingBoxesCanvas.getContext("2d");
+
+	previewCtx.lineWidth = 1;
+	boxesCtx.lineWidth = 1;
+
+	let isDown = false;
+	let currentBox = null;
+
+	DrawingPreviewCanvas.addEventListener("mousedown", (e) => {
+		isDown = true;
+		currentBox = {
+			type: GetDrawType(),
+			start: {
+				x: e.offsetX,
+				y: e.offsetY
+			}
+		};
+		currentBox.end = currentBox.start;
+	});
+
+	DrawingPreviewCanvas.addEventListener("mousemove", (e) => {
+		if(isDown) {
+			currentBox.end = {
+				x: e.offsetX,
+				y: e.offsetY
+			};
+		}
+	});
+
+	DrawingPreviewCanvas.addEventListener("mouseleave", () => {
+		isDown = false;
+	});
+
+	DrawingPreviewCanvas.addEventListener("mouseup", (e) => {
+		isDown = false;
+		if(currentBox && currentBox.end !== currentBox.start) {
+			onBoxDraw(currentBox);
+			currentBox = null;
+		}
+	});
+
+	function GetDrawType() {
+		return HitboxBrushButton.classList.contains("ms-action") ? HITBOX : HURTBOX;
+	}
+
+	function RenderBoxes() {
+		requestAnimationFrame(RenderBoxes);
+		previewCtx.clearRect(0, 0, previewCtx.canvas.width, previewCtx.canvas.height);
+		boxesCtx.clearRect(0, 0, boxesCtx.canvas.width, boxesCtx.canvas.height);
+
+		let boxes = getBoxes();
+		for(let box of boxes) {
+			if(box.type === HITBOX) {
+				boxesCtx.strokeStyle = "red";
+			} else {
+				boxesCtx.strokeStyle = "lime";
+			}
+			let w = box.end.x - box.start.x;
+			let h = box.end.y - box.start.y;
+			boxesCtx.strokeRect(box.start.x - 0.5, box.start.y - 0.5, w, h);
+		}
+
+		if(!currentBox || !currentBox.end) return;
+
+		let w = currentBox.end.x - currentBox.start.x;
+		let h = currentBox.end.y - currentBox.start.y;
+
+		if(currentBox.type === HITBOX) {
+			previewCtx.strokeStyle = "red";
+		} else {
+			previewCtx.strokeStyle = "lime";
+		}
+
+		previewCtx.strokeRect(currentBox.start.x - 0.5, currentBox.start.y - 0.5, w, h);
+	}
+
+	requestAnimationFrame(RenderBoxes);
+}
--- site/js/game.js
+++ site/js/game.js
@@ -1,25 +1,84 @@
 window.addEventListener("load", () => {
 	let DrawingContainer = document.getElementById("DrawingContainer");
 	let DrawingImgDesc = document.getElementById("DrawingImgDesc");
-	let DrawingCanvas = document.getElementById("DrawingCanvas");
+	let DrawingBGCanvas = document.getElementById("DrawingBGCanvas");
+	let DrawingBoxesCanvas = document.getElementById("DrawingBoxesCanvas");
+	let DrawingPreviewCanvas = document.getElementById("DrawingPreviewCanvas");
+	let TimeOverlay = document.getElementById("TimeOverlay");
 	let HitboxBrushButton = document.getElementById("HitboxBrushButton");
 	let HurtboxBrushButton = document.getElementById("HurtboxBrushButton");
 	let UndoButton = document.getElementById("UndoButton");
 
-	const CANVAS_PADDING = 30;
+	const CANVAS_PADDING = 50;
 
-	let ctx = DrawingCanvas.getContext("2d");
+	let currentBoxes = [];
+	let manager = DrawManager(onBoxDraw, getCurrentBoxes);
+	let ctx = DrawingBGCanvas.getContext("2d");
+
+	HitboxBrushButton.addEventListener("click", () => {
+		HitboxBrushButton.classList.add("ms-action");
+		HurtboxBrushButton.classList.remove("ms-action");
+	});
+
+	HurtboxBrushButton.addEventListener("click", () => {
+		HurtboxBrushButton.classList.add("ms-action");
+		HitboxBrushButton.classList.remove("ms-action");
+	});
+
+	UndoButton.addEventListener("click", () => {
+		currentBoxes.splice(currentBoxes.length - 1, 1);
+	});
 
 	window.SOCKET.on("game start", (data) => {
-		DrawingContainer.classList.remove("d-none");
+		currentBoxes = [];
 
 		LoadImage(`/img/sprites/${data.char}/none/${data.move}.png`, (img) => {
+			DrawingContainer.classList.remove("d-none");
 			DrawingImgDesc.innerText = `${data.char}: ${ParseMove(data.move)}`;
-			DrawingCanvas.width = img.width + CANVAS_PADDING + CANVAS_PADDING;
-			DrawingCanvas.height = img.height + CANVAS_PADDING + CANVAS_PADDING;
+			let w = img.width + CANVAS_PADDING + CANVAS_PADDING;
+			let h = img.height + CANVAS_PADDING + CANVAS_PADDING;
+			DrawingBGCanvas.width = w;
+			DrawingBGCanvas.height = h;
+			DrawingBoxesCanvas.width = w;
+			DrawingBoxesCanvas.height = h;
+			DrawingPreviewCanvas.width = w;
+			DrawingPreviewCanvas.height = h;
+			DrawingBGCanvas.parentElement.style.width = `${w}px`;
+			DrawingBGCanvas.parentElement.style.height = `${h}px`;
 			ctx.drawImage(img, CANVAS_PADDING, CANVAS_PADDING);
+
+			ShowTimedown();
 		});
 	});
+
+	function onBoxDraw(box) {
+		console.log("box", box);
+		currentBoxes.push(box);
+	}
+
+	function getCurrentBoxes() {
+		return currentBoxes;
+	}
+
+	function ShowTimedown() {
+		let timer = null;
+		let startTime = Date.now();
+		TimeOverlay.classList.remove("d-none");
+		setTimeout(() => {
+			clearInterval(timer);
+			TimeOverlay.innerText = "Go!";
+			TimeOverlay.style.opacity = 0;
+			setTimeout(() => {
+				TimeOverlay.style.opacity = 1;
+				TimeOverlay.classList.add("d-none");
+				TimeOverlay.innerText = "";
+			}, 950);
+		}, 3000);
+		timer = setInterval(() => {
+			let sec = Math.ceil((Date.now() - startTime) / 1000);
+			TimeOverlay.innerText = sec + "!";
+		});
+	}
 
 	function LoadImage(src, cb) {
 		let img = new Image();
